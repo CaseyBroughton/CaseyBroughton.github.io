c7cd94b391efe06d97dbb33a9581fc206d97daf4
CaseyBroughton <Casey@Casey-Broughtons-MacBook.local>
2015-08-02 15:57:15 -0700

    Changed a page title

--- Elect_Stephen_Harper/Home_files/Home.js
+++ Elect_Stephen_Harper/Home_files/Home.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{loadMozillaCSS('Index_files/IndexMoz.css')
+{loadMozillaCSS('Home_files/HomeMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
