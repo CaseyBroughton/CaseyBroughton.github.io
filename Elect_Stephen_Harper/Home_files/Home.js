187e0363b5da7c1fc9b11bb19278199c45ecc592
CaseyBroughton <Casey@Casey-Broughtons-MacBook.local>
2015-10-19 20:56:29 -0700

    Harper Lost
    Website Shutdown Message

--- Elect_Stephen_Harper/Home_files/Home.js
+++ Elect_Stephen_Harper/Home_files/Home.js
@@ -1,9 +1,11 @@
-// Created by iWeb 3.0.2 local-build-20151011
+// Created by iWeb 3.0.2 local-build-20151019
 
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Home_files/HomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
